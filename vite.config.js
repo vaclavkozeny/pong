c824d3fb45046a3395bc62e0049b3ea8ee79e1f9
Václav Kožený <vaclav.kozeny@tul.cz>
2026-01-08 14:39:58 +0100

    workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/nazev-tveho-repozitare/',
+    base: '/pong/',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
